08693042736eee8e65a968831e1edb623cf1dc62
Robson <contato@gt8.com.br>
2013-01-15 14:36:34 -0800

    Comentada a linha que mostra o ícone 'Novo Diretório'.

--- engine/views/admin/explorer/levels/banners/index.js
+++ engine/views/admin/explorer/levels/banners/index.js
@@ -3,7 +3,7 @@
 			
 			jCube(function(){
 				jCube('::h1').setHTML('Banners');
-				jCube('::nav.toolbar a.new-folder').addClass('hidden');
+				//jCube('::nav.toolbar a.new-folder').addClass('hidden');
 				jCube('::nav.toolbar a.new-folder img').setProperty('title', 'Criar novo banner');
 				Explorer.strings.label	= 'Novo banner';
 			});
