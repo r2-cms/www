85f97c80603db8bc0bd2f12bd3df42e467a7fcf3
Robson <contato@gt8.com.br>
2013-01-20 07:53:34 -0800

    Adicionado funções para adicao e recuperação de domínios.

--- engine/controllers/admin/security/scanner/Index.php
+++ engine/controllers/admin/security/scanner/Index.php
@@ -6,34 +6,81 @@
 	require_once( SROOT ."engine/classes/CardLister.php");
 	require_once( SROOT ."engine/queries/security/ScanDomain.php");
 	
-	class Index extends CardLister {
+	class Index extends ScanDomain {
 		public $name	= 'security-scanner/';
 		public $tableType	= null;
+		private $fieldsDefault = array("id", "id_user", "ftp", "domain", "login", "pass", "port", "scan_frequency", "creation", "modification");
+		protected $fields = array();
+		protected $format = "";
+		protected $where = "";
+		protected $clauseAnd = "";
+		protected $limit = "";
+		protected $index = "";
+		protected $group = "";
 		
 		function __construct() {
+			parent::__construct();
 			global $GT8;
-			parent::CardLister();
 			
 			$this->checkActionRequest();
 			$this->checkReadPrivileges();
+			$fieldsArgs = array();
+			$fieldsArgs[] = isset($_GET['fields']) & $_GET['fields']!=null & $_GET['fields']!=""? count(explode(",", $_GET['fields']))>0? explode(",", $_GET['fields']): "": "";
+			$fieldsDiff = array_diff($fieldsArgs, $this->fieldsDefault);
 			
+			foreach($fieldsDiff as $key=>$value){
+				for($i=0; $i<count($fieldsArgs); $i++){
+					unset($fieldsArgs[$key]);
+				}
+			}
+			$fieldsArgs = array_values($fieldsArgs);
 			
-			//teste
-			$ScanDomain = new ScanDomain();
-			$ScanDomain->getDomains(
-				array(field=>array(id=>'id', name=>'name', creation=>)'creation')
-			);
+			for($i=0; $i<count($this->fieldsDefault); $i++){
+				for($j=0; $j<count($fieldsArgs); $j++){	
+					if($this->fieldsDefault[$i] === $fieldsArgs[$j]){
+						$this->fields[$this->fieldsDefault[$i]] = $fieldsArgs[$j];
+					}
+				}
+			}
+			
+			$this->format = isset($_GET['format'])? mysql_real_escape_string($_GET['format']): "object";
+			$this->where = isset($_GET['where'])? mysql_real_escape_string($_GET['where']): "";
+			$this->clauseAnd = isset($_GET['and'])? mysql_real_escape_string($_GET['and']): "";
+			$this->limit = isset($_GET['limit'])? (integer)($_GET['limit']): 50;
+			$this->index = isset($_GET['index'])? (integer)($_GET['index']): 0;
+			$this->group = isset($_GET['group'])? mysql_real_escape_string($_GET['group']): "";
+			//$this->returnScanDomain();
+			$this->addScanDomain();
 		}
+		
 		public function returnScanDomain(){
 			//&action=new-domain&value=elefante.com.br
-			$Pager	= Pager( array(
-				'sql'		=> 'security.listDomains'
-			));
-			$this->data['domains'] = $Pager['rows'];
+			$getDomains = $this->getDomains(
+				array(
+					field=>$this->fields,
+					clauseWhere=>$this->where,
+					clauseAnd=>$this->clauseAnd,
+					limit=>$this->limit,
+					index=>$this->index,
+					group=>$this->group,
+					format=>$this->format
+				)
+			);
+			
+			$this->data['domains'] = $getDomains['rows'];
+			print_r($this->data['domains']);
+			die('admin/security/scanner/Index.php');
 			return $this->data['domains'];
 		}
 		
-		
+		public function addScanDomain(){
+			print(
+				$this->addDomains(
+					array(creation=>"2013/01/20")
+				)	
+			);
+			die();
+		}		
 		public function on404() {
 			if ( !$this->id ) {
 				parent::on404();
