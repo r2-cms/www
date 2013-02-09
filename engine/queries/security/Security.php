a626c2823ac695f28f4a76fb9e8ac12eab10cd9c
Robson <contato@gt8.com.br>
2013-02-08 21:34:27 -0800

    Em processo de finalização. Pelo menos das ideias iniciais.

--- engine/queries/security/Security.php
+++ engine/queries/security/Security.php
@@ -2,7 +2,45 @@
 	require_once( SROOT .'engine/classes/GT8.php');
 	
 	class Security  extends GT8{
-		
+		public function __construct(){
+			
+		}
+		public function validField($field, $table, $alias){
+			$position = 0;
+			$length = 0;
+			$this->name = $table;
+			$Field = null;
+			
+			if(count(explode(",", $field))> 0){
+				$field = explode(",", $field);
+				$position = 0;
+				$length = 0;
+				
+				for($i=0; $i<count($field); $i++){
+					$field[$i] = ltrim($field[$i]);
+					
+					if(substr($field[$i], 0, 6) == "COUNT("){
+						$position = 6 + strlen(RegExp($alias, '[a-zA-Z0-9]+'))+1;
+						$length = strpos(substr($field[$i], $position), ")");
+						//Conforme a necessidade pode-se adicionar mais condicionais para encontrar campos dentro de funções do mysql
+					}else{
+						$position = strlen(RegExp($alias, '[a-zA-Z0-9]+'))+1;
+						if(strpos(substr($field[$i], $position), " ")){
+							$length = strpos(substr($field[$i], $position), " ");
+						}elseif(strpos(substr($field[$i], $position), ",")){
+							$length = strpos(substr($field[$i], $position), ",");
+						}else{
+							$length = strpos(substr($field[$i], $position), substr($field[$i], -1)) + 1;
+						}
+					}
+					$Field = $this->getType(RegExp(substr($field[$i], $position, $length),  '[a-zA-Z0-9_\-\.\s]+'));
+					if(!$Field){
+						print("//#error: Campo " . strtoupper(substr($field[$i], $position, $length)) . " não encontrado em " . $this->name .  "!");
+					}
+				}
+			}
+			return $Field;
+		}
 	}
 	interface IScanDomain{
 		public function getDomains($props = array());
