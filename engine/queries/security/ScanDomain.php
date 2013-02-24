fd1789f17fab87216ab4f52530d6a889abe12234
Robson <contato@gt8.com.br>
2013-02-23 21:32:14 -0800

    Realizado alterações no método updateDomains, permitindo que se atualiza mais de um campo por vez. Realizado também alterações no método getDomains, adicionando uma opção para busca com a cláusula like, porém ainda serão necessárias algumas melhorias para funcionar 100%.

--- engine/queries/security/ScanDomain.php
+++ engine/queries/security/ScanDomain.php
@@ -13,11 +13,21 @@
      * 					gt8_users = u;
      * 				Caso o parâmetro $props['field'] venha indefinido a propriedade $sqlFull será passada como padrão;
      *
+     * 			- $props['searchLike']
+     * 				Function: retorna resultados que contenham TODAS as procuras coincidentes
+     * 				Parameters: prefix.field%value. Ex.: 'sd.domain%www.r2-cms.com.br'
+     * 				Obs.: Se houver mais de uma procura elas deverão ser passadas separadas por vírgulas.
+     * 					ex.: 'sd.domain%www.r2cms.com.br, sd.ftp%ftp@r2cms.com.br'
+     *
      * 	@method: updateDomains()
      * 		@params:
      * 			- $id (id do domínio à ser atualizado)
-     * 			- $field (campo a ser atualizado)
-     * 			- $value (valor de field. Valor do campo a ser atualizado)
+     * 			- $field (campo ou campos a serem atualizados)
+     * 				Obs.: caso haja mais de um campo FIELD a ser atualizado os campos deverão ser passados separados por vírgulas,
+     * 				e os valores para VALUE deverão ser também separados por vírgulas, e com a quantidade equivalente a quantidade de campos informados.
+     * 			- $value (valor ou valores de field. Valores dos campos a serem atualizados)
+     * 				Ex.: 	$props['field'] = 'campo1, campo2, campo3, campo4'
+     * 						$props['value']	= 'valor1, valor2, campo3, campo4'
      * 			- $format (formato de retorno [OBJECT,TABLE,CARD,JSON,GRID,TEMPLATE])
     **/
 
@@ -44,6 +54,8 @@
 		public function getDomains($props = array()){
 			$format = in_array($props['format'], explode(',', 'OBJECT,TABLE,CARD,JSON,GRID,TEMPLATE'))? $props['format']: 'OBJECT';
 			$field = isset($props['fields']) && $props['fields']? $props['fields']: $this->sqlFull;
+			$like = isset($props['searchLike']) && $props['searchLike']? $props['searchLike']: null;
+			$searchLike = array();
 			$fields = null;
 			$where = "";
 			$limit = isset($props['limit'])? (integer)$props['limit']: 50;
@@ -101,7 +113,63 @@
 			if(isset($props['clauseAnd'])){
 				$where .= mysql_real_escape_string($props['clauseAnd']) . PHP_EOL;
 			}
-			
+			if(isset($like) && $like){
+				$like = explode(',', $like);
+				$slike = null;
+				$sfield = null;
+				$svalue = null;
+				
+				if(count($like)>0){
+					for($i=0; $i<count($like); $i++){
+						$slike = explode('%', $like[$i]);
+						
+						if(count($slike)>1){
+							if(substr($slike[0], 0, 3) == "sd."){
+								$table = "scan_domains";
+								$alias = "sd";
+							}
+							if(substr($slike[0], 0, 2) == "u."){
+								$table = "users";
+								$alias = "u";
+							}
+							if($this->validField($slike[0], $table, $alias)){
+								$sfield .= $slike[0] . ", ";
+								$svalue .= $slike[1] . PHP_EOL;
+							}
+						}
+					}
+					
+					//Obs.: a considerar com o Roger:
+					//Na function Page option [search], na primeira chave do array, quando os campos
+					//ou os campos são enviados entre aspas simples ocorre um erro conforme exemplificado abaixo:
+					/*SELECT
+						sd.id,sd.domain
+					FROM	
+							gt8_scan_domains sd
+								INNER JOIN
+									gt8_users u
+										ON
+											u.id = sd.id_user				
+					WHERE
+						1 = 1
+						AND (
+							1 = 0
+							OR 'sd.domain' LIKE '%'192.168.0.10'%'
+						)
+					LIMIT
+						0, 50
+					ERROR:You have an error in your SQL syntax; check the manual that corresponds to your
+					MySQL server version for the right syntax to use near 	'192.168.0.10'%' ) LIMIT 0, 50' at line 15
+					*/
+					//OR 'sd.domain' LIKE '%'192.168.0.10'%': o campo fica entre aspas sendo encarado como uma string, dando erro
+					//
+					
+					//print(count(explode(",", substr(trim($sfield), 0, -1)))>1? "YES":"NO");
+					//die();
+					
+					$searchLike[] = array(substr(trim($sfield), 0, -1), trim($svalue));
+				}
+			}
 			$Pager = Pager(array(
 				'select' => $fields,
 				'from' => '
@@ -112,6 +180,7 @@
 									u.id = sd.id_user
 				',
 				'where' => $where,
+				'search' => $searchLike,
 				'index' => $index,
 				'limit' => $limit,
 				'group' => $group,
@@ -155,7 +224,7 @@
 						'$creation',
 						'$modification'
 					)
-			") or die($_SESSION['login']['level']>7? "//#error: SQL INSERT Error:". mysql_error() . PHP_EOL : '//#error: Erro ao acessar o banco de dados!'. PHP_EOL);
+			")or die($_SESSION['login']['level']>7? "//#error: SQL INSERT Error:". mysql_error() . PHP_EOL : '//#error: Erro ao acessar o banco de dados!'. PHP_EOL);
 			$id = mysql_insert_id();
 			
 			if($id){
@@ -167,75 +236,94 @@
 			require_once( SROOT .'engine/classes/Update.php');
 			
 			$this->id_domain = (integer)$id;
-			$field = isset($props['field'])? RegExp($props['field'], '[a-zA-Z_\-]+'): null;
-			$value = isset($props['value'])? mysql_real_escape_string($props['value']): null;
-			$FieldValue = isset($props['FieldValue'])? $props['FieldValue']: null;
+			$field = isset($props['field'])? explode(",", $props['field']): false;
+			$value = isset($props['value'])? explode(",", $props['value']): false;
 			$format = in_array($props['format'], explode(',', 'OBJECT,TABLE,CARD,JSON,GRID'))? $props['format']: 'OBJECT';
+			$fieldValue = array();
 			
 			if(!$this->id_domain || $this->id_domain==0){
 				print('//#error: ID do domínio é obrigatório!'. PHP_EOL);
 				die();
 			}
-			if($field === "id_user"){
-				$value = (integer)$value;
-				if(!$value || $value<1){
-					print('//#error: valor para o campo ' . $field . ' não definido!');
-					die();
-				}
-			}
-			if($field === "ftp"){
-				if(!$value){
-					print("//#error: valor para o campo ' . $field . ' não definido!");
-					die();
-				}
-				$value = RegExp($value, '[a-zA-Z@\-\.\_0-9]+');
-			}
-			if($field === "domain"){
-				print("//#error: Esse campo não pode ser alterado!");
-				die();
-			}
-			if($field === "login"){
-				print("//#error: Esse campo não pode ser alterado!");
-				die();
-			}
-			if($field === "pass"){
-				if(!$value){
-					print("//#error: valor para o campo ' . $field . ' não definido!");
-					die();
-				}
-			}
-			if($field === "port"){
-				$value = (integer)$value;
-				if(!$value || $value==0){
-					$value = 21;
-				}
-			}
-			if($field === "scan_frequency"){
-				$value = (integer)$value;
-				if($value || $value==0){
-					$value == 4;
-				}
-			}
-			if($field === "creation"){
-				//print("//#error: Esse campo não pode ser alterado!");
-				//die();
-			}
-			if($field === "modification"){
-				$value = date("Y-m-d H:i:s");
-			}
-			$this->args['id'] = $this->id_domain;
-			$this->args['field'] = $field;
-			$this->args['value'] = $value;
-			$this->args['format'] = 'OBJECT';
-			$this->args['privilegeName'] = $this->privilegeName;
-			$this->args['name'] = 'scan_domains';
-			$Update = new Update($this->args);
 			
-			if($Update){
-				print("//#message: Registro alterado com sucesso!");
-				die();
+			if($field && count($field)>0){
+				if(count($value) < count($field) || count($value) > count($field)){
+					print("#error: Quantidade de valores incompatíveis com a quantidade de campos informados!");
+					die();
+				}
+				$fieldValue = array_combine($field, $value);
+				
+				foreach($fieldValue as $key=>$vl){
+					$field = RegExp($key, '[a-zA-Z_\-]+');
+					$value = mysql_real_escape_string($vl);
+					
+					if($field === "id_user"){
+						$value = (integer)$value;
+						if(!$value || $value<1){
+							print('//#error: valor para o campo ' . $field . ' não definido!');
+							die();
+						}
+					}
+					if($field === "ftp"){
+						if(!$value){
+							print("//#error: valor para o campo ' . $field . ' não definido!");
+							die();
+						}
+						$value = RegExp($value, '[a-zA-Z@\-\.\_0-9]+');
+					}
+					if($field === "domain"){
+						print("//#error: Esse campo não pode ser alterado!");
+						die();
+					}
+					if($field === "login"){
+						print("//#error: Esse campo não pode ser alterado!");
+						die();
+					}
+					if($field === "pass"){
+						if(!$value){
+							print("//#error: valor para o campo ' . $field . ' não definido!");
+							die();
+						}
+					}
+					if($field === "port"){
+						$value = (integer)$value;
+						if(!$value || $value==0){
+							$value = 21;
+						}
+					}
+					if($field === "scan_frequency"){
+						$value = (integer)$value;
+						if(!$value || $value==0){
+							$value == 4;
+						}
+					}
+					if($field === "creation"){
+						//print("//#error: Esse campo não pode ser alterado!");
+						//die();
+					}
+					if($field === "modification"){
+						if(!$value){
+							$value = date("Y-m-d H:i:s");
+						}
+					}
+					$this->args['id'] = $this->id_domain;
+					$this->args['field'] = $field;
+					$this->args['value'] = $value;
+					$this->args['format'] = $format;
+					$this->args['privilegeName'] = $this->privilegeName;
+					$this->args['name'] = 'scan_domains';
+					$Update = new Update($this->args);
+					
+					if($Update){
+						print("//#message: Registro alterado com sucesso!");
+						die();
+					}else{
+						print("//#error: Erro ao alterar registro!");
+						die();
+					}
+				}
 			}else{
-				print("//#error: Erro ao alterar registro!");
+				print("#error: Nenhum campo foi informado para ser atualizado!");
 				die();
 			}
 		}
